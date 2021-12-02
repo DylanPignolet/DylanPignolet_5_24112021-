55ac9efbe6e102b9ea59f8294ef8eca0b6131fd7
Dylan Pignolet <dylan.pignolet2@gmail.com>
2021-12-02 14:10:58 +0100

    début url id par produit

--- front/js/product.js
+++ front/js/product.js
@@ -1,7 +1,7 @@
 const params = new URLSearchParams(window.location.search)
 let _id = params.get('_id');
 
-function product() {
+
     fetch('http://localhost:3000/api/'+ _id)
         .then((response) => {
             return response.json()
